62476e6c1296f02e2ae052b3cae2bc6e6d651604
Havra <krystof@havrak.xyz>
2019-03-02 16:42:40 +0100

    Oraganized files

--- .scripts/i3/randrDualScreen.sh
+++ .scripts/i3/randrDualScreen.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1080 --pos 1366x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 0x384 --rotate normal --output DP-2 --off
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 2560x432 --rotate normal --output DP-2 --off
 sleep 1
 setbg
-
-
 
