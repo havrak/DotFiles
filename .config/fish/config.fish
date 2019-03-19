285e4bb25bbe6787acd8be118866fdb74942e507
Havra <krystof@havrak.xyz>
2019-03-19 11:05:57 +0100

    Exported variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,26 +1,31 @@
 #!/usr/bin/fish
 
 set PATH (du "$HOME/.scripts/" | cut -f2 | tr '\n' ':') $PATH
+set -x READER zathura
+set -x EDITOR nvim
+set -x BROWSER firefox
+
 fish_vi_key_bindings
 function fish_mode_prompt
 end
 function fish_greeting
 end
 function fish_prompt
-	set_color red --bold
-	printf "["
-	set_color yellow
+	set_color yellow --bold
 	printf "%s" "$USER"
 	set_color green
 	printf "@"
 	set_color blue
 	printf "%s" "$hostname"
 	set_color magenta
-	printf " "
+	printf ":"
 	printf (basename $PWD)
 	set_color red
-	printf "]"
+	printf "-"
+	printf ">"
 	set_color normal
+	# del key does not work properly by itself
+	printf '\033[?1h\033=' >/dev/tty
 end
 
 abbr p "yaourt"
@@ -29,7 +34,7 @@ abbr orphans "yaourt -Qqtd"
 abbr sr "sudo ranger"
 abbr r "ranger"
 abbr c "clear"
-abbr sv "sudoedit"
+abbr sv "sudo nvim"
 abbr grp "git remote | xargs -L1 git push --all"
 abbr g "git"
 abbr music "ncmpcpp"
@@ -37,4 +42,5 @@ abbr z "zathura --fork"
 abbr s "sxiv"
 abbr java8jar "/usr/lib/jvm/java-8-jdk/jre/bin/java -jar"
 abbr java8 "/usr/lib/jvm/java-8-jdk/jre/bin/java"
+
 
