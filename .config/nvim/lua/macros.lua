723bb02ecb94c7fd7661524ea2fbbbf08fd2dd87
Havra <krystof@havrak.xyz>
2024-11-04 16:00:22 +0100

    Moved to another plugin in nvim to provide copilot functionality

--- .config/nvim/lua/macros.lua
+++ .config/nvim/lua/macros.lua
@@ -40,7 +40,8 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set('i', ',ct', '\\textcite{}<Esc>T{i') -- textcite
 		vim.keymap.set('i', ',ci', '\\fullbibentry{}<Esc>T{i') -- fullbibentry
 		vim.keymap.set('i', ',cf', '\\footnote{Ibid.<space>\\cite{<++>}:<space><++>}') -- footnote with citation
-		vim.keymap.set('i', ',ref', '\\ref{}<Esc>T{i') -- reference
+		vim.keymap.set('i', ',r', '\\ref{}<Esc>T{i') -- reference to label
+		vim.keymap.set('i', ',er', '\\eqref{}<Esc>T{i') -- reference to equation
 
 		-- LaTeX Lists
 		vim.keymap.set('i', ',ul', '\\begin{itemize}<Enter><Enter>\\end{itemize}<Esc>kA\\item<Space>') -- unordered list
@@ -84,6 +85,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set('i', '.cZ', '\\mathscr{Z}^{-1}[](<++>)<Space><++><Esc>11hi') -- Z transform
 		vim.keymap.set('i', '.ch', '\\mathbbm{1}') -- Heaviside function
 		vim.keymap.set('i', '.arr', '\\left\\{\\begin{array}{} <++> \\end{array} \\right.<Esc>25hi') -- to display multiple possibilities with given conditions
+		vim.keymap.set('i', '.eq', '\\begin{equation}<Enter><Enter>\\label{eq:<++>}<Enter>\\end{equation}<Esc>2k$i') -- equation
 
 		-- LaTeX Beamer specific
 		vim.keymap.set('i', ',frames', '\\begin{frame}[fragile]<Enter>\\frametitle{}<Enter>\\begin{itemize}<Enter>\\item <++><Enter>\\end{itemize}<Enter>\\end{frame}<Enter><Enter><++><Esc>6kf}i') -- frame
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -15,19 +15,58 @@ return {
 
 { 'preservim/tagbar', event = "VeryLazy"},
 
-{ 'github/copilot.vim', event = "VeryLazy", config = function()
-	vim.g.copilot_enabled = 1
-	vim.g.copilot_assume_mapped = 1
-end},
+-- { 'github/copilot.vim', event = "VeryLazy", config = function()
+	--	vim.g.copilot_enabled = 1
+	--	vim.g.copilot_assume_mapped = 1
+	-- end},
+	{
+		"zbirenbaum/copilot.lua",
+		event = "VeryLazy",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = true,
+					auto_refresh = true,
+						layout = {
+							position = "bottom"
+							ratio = 0.3
+						},
+					},
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+						hide_during_completion = false,
+						debounce = 50,
+						keymap = {
+							accept = "<S-CR>",
+							accept_word = false,
+							accept_line = false,
+						},
+					},
+					filetypes = {
+						cpp = true,
+						latex = true,
+						markdown = true,
+						vimwiki = true,
+						["."] = true,
+					},
+					copilot_node_command = 'node', -- Node.js version must be > 18.x
+					server_opts_overrides = {
+						enable = true,
+						inlineSuggest = {
+							enable = true,
+						}},
+					})
+				end,
+			},
+			{ 'Chiel92/vim-autoformat', event = "VeryLazy", config = function()
+				vim.g.formatdef_latexindent = '"latexindent --logfile=/dev/null --local=$HOME/.config/latexindent/config.yaml -"'
+			end},
 
-{ 'Chiel92/vim-autoformat', event = "VeryLazy", config = function()
-	vim.g.formatdef_latexindent = '"latexindent --logfile=/dev/null --local=$HOME/.config/latexindent/config.yaml -"'
-end},
-
-{'aperezdc/vim-template' , event = "VeryLazy", config = function()
-	vim.g.email="krystof@havrak.xyz"
-	vim.g.username="Havránek Kryštof"
-	vim.g.templates_directory= '$HOME/.config/nvim/templates'
-	vim.g.templates_no_autocmd = 0
-end}
-}
+			{'aperezdc/vim-template' , event = "VeryLazy", config = function()
+				vim.g.email="krystof@havrak.xyz"
+				vim.g.username="Havránek Kryštof"
+				vim.g.templates_directory= '$HOME/.config/nvim/templates'
+				vim.g.templates_no_autocmd = 0
+			end}
+		}
