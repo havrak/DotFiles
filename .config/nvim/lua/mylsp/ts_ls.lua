81e4e67a995b763a978b3c1a634e946cb814ec1c
Havra <krystof@havrak.xyz>
2026-01-26 13:32:40 +0100

    Added typescript/nodejs language server to nvim

---
+++ .config/nvim/lua/mylsp/ts_ls.lua
@@ -0,0 +1,40 @@
+return {
+    root_markers = {
+        "package.json",
+        "tsconfig.json",
+        "jsconfig.json",
+        ".git",
+    },
+    -- Optional: If you use Prettier/Eslint for formatting, set this to false
+    format = true,
+    settings = {
+        javascript = {
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+            },
+            suggest = {
+                completeFunctionCalls = true,
+            },
+        },
+        typescript = {
+            inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+            },
+            suggest = {
+                completeFunctionCalls = true,
+            },
+        },
+    },
+}
